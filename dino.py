dadeff19595a972cb8f2260bd88b39e209d142f3
Maverickwarrior <patrickcumminsyeah@gmail.com>
2018-09-15 17:59:15 +0530

    dino succesful

---
+++ dino.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import time
+from PIL import Image
+from os import environ
+import cv2
+import numpy as np
+import pyautogui as pg
+pg.PAUSE = 0
+
+#for mac or windows users:
+from PIL import ImageGrab
+#for ubuntu users:
+#import pyscreenshot as ImageGrab
+
+
+
+#(x,y) to represent left-top corner of the rectangle, little away from dino
+#(x+w,y+h) bottom-right corner of the rectangle enclosing dino and coming cactus.
+x, y, w, h = 585, 327, 244, 100
+while True:
+    img = ImageGrab.grab()
+
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+
+    img = img[ y:y+h, x:x+w ]
+
+    for x0 in range(170,240,5):
+        px = img[58,x0]
+        if (px[0]<=150):
+            print("jump")
+            pg.press("up")
+            time.sleep(0.05)
+            break
